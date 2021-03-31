d01ef4601fed0d884daefb2fb2f9b2652ea8b75b
muddssarshaikh <muddssarshaikh@hotmail.com>
2021-03-31 15:43:22 +0530

    added new component test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentTestComponent } from './component-test/component-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentTestComponent
   ],
   imports: [
     BrowserModule,
